cc1cef1135c09704103ac29e957c226505912b5f
Tanishka Pandarpatte <tanishkapandarpatte2302@gmail.com>
2026-01-05 21:00:36 +0530

    Fix Google OAuth callback URL for production

--- backend/passport.js
+++ backend/passport.js
@@ -6,7 +6,9 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+     // callbackURL: "/auth/google/callback",
+     callbackURL: "https://bg-verification-chat.onrender.com/auth/google/callback",
+
     },
     (accessToken, refreshToken, profile, done) => {
       return done(null, {
