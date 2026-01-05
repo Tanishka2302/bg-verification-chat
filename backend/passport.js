2fc57285b38569a5044fef85d40c294618c8edcd
Tanishka Pandarpatte <tanishkapandarpatte2302@gmail.com>
2026-01-06 01:42:53 +0530

    Fix GoogleStrategy callbackURL configuration

--- backend/passport.js
+++ backend/passport.js
@@ -6,8 +6,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:
-        "https://bg-verification-chat.onrender.com/auth/google/callback",
+      callbackURL: "https://bg-verification-chat.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
